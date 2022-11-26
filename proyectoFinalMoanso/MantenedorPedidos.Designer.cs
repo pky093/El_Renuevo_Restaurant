f75eb581cd6c6e077b6616de2536c3842d33342a
miguel enrique lorenzo torres <miguel_14_21_14@hotmail.com>
2022-11-26 02:22:21 -0500

    xd

--- proyectoFinalMoanso/MantenedorPedidos.Designer.cs
+++ proyectoFinalMoanso/MantenedorPedidos.Designer.cs
@@ -173,7 +173,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(407, 87);
+            this.pictureBox1.Location = new System.Drawing.Point(399, 78);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(245, 166);
