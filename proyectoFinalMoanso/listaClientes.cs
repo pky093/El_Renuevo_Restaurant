948eea6c8f5ed211be6f280510f5ccb8ea3f9232
Jonatan Laureano <N00176041@upn.pe>
2022-11-27 19:14:15 -0500

    actualizacion

--- proyectoFinalMoanso/listaClientes.cs
+++ proyectoFinalMoanso/listaClientes.cs
@@ -17,5 +17,9 @@ namespace proyectoFinalMoanso
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //xd
+        }
     }
 }
