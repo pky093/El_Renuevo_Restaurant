79bd92b742087132d3a6796f0cdb5bab6fd52a0e
Jonatan Laureano <N00176041@upn.pe>
2022-11-30 02:52:42 -0500

    pedidos

--- proyectoFinalMoanso/MantenedorPedidos.Designer.cs
+++ proyectoFinalMoanso/MantenedorPedidos.Designer.cs
@@ -51,9 +51,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Mongolian Baiti", 23.25F, System.Drawing.FontStyle.Bold);
-            this.label3.Location = new System.Drawing.Point(242, 223);
+            this.label3.Location = new System.Drawing.Point(182, 181);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(116, 41);
+            this.label3.Size = new System.Drawing.Size(95, 33);
             this.label3.TabIndex = 3;
             this.label3.Text = "Plato:";
             this.label3.UseWaitCursor = true;
@@ -62,9 +63,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 23.25F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(211, 282);
+            this.label1.Location = new System.Drawing.Point(158, 229);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(147, 41);
+            this.label1.Size = new System.Drawing.Size(121, 33);
             this.label1.TabIndex = 4;
             this.label1.Text = "Bebida:";
             this.label1.UseWaitCursor = true;
@@ -73,9 +75,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Mongolian Baiti", 23.25F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(240, 432);
+            this.label2.Location = new System.Drawing.Point(180, 351);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(118, 41);
+            this.label2.Size = new System.Drawing.Size(97, 33);
             this.label2.TabIndex = 5;
             this.label2.Text = "Total:";
             this.label2.UseWaitCursor = true;
@@ -84,9 +87,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Mongolian Baiti", 23.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(35, 27);
+            this.label4.Location = new System.Drawing.Point(26, 22);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(204, 41);
+            this.label4.Size = new System.Drawing.Size(167, 33);
             this.label4.TabIndex = 6;
             this.label4.Text = "ID Cliente:";
             this.label4.UseWaitCursor = true;
@@ -100,10 +104,10 @@
             this.comboBox1.Items.AddRange(new object[] {
             "cabrito",
             "arroz con pollo"});
-            this.comboBox1.Location = new System.Drawing.Point(373, 227);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Location = new System.Drawing.Point(280, 184);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(309, 37);
+            this.comboBox1.Size = new System.Drawing.Size(233, 30);
             this.comboBox1.TabIndex = 7;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
@@ -115,10 +119,10 @@
             this.comboBox2.Items.AddRange(new object[] {
             "gaseosas personales",
             "inka de litro"});
-            this.comboBox2.Location = new System.Drawing.Point(373, 289);
-            this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox2.Location = new System.Drawing.Point(280, 235);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(309, 37);
+            this.comboBox2.Size = new System.Drawing.Size(233, 30);
             this.comboBox2.TabIndex = 8;
             this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
@@ -126,9 +130,10 @@
             // 
             this.labelidCliente.AutoSize = true;
             this.labelidCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F);
-            this.labelidCliente.Location = new System.Drawing.Point(259, 27);
+            this.labelidCliente.Location = new System.Drawing.Point(194, 22);
+            this.labelidCliente.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelidCliente.Name = "labelidCliente";
-            this.labelidCliente.Size = new System.Drawing.Size(118, 42);
+            this.labelidCliente.Size = new System.Drawing.Size(95, 36);
             this.labelidCliente.TabIndex = 9;
             this.labelidCliente.Text = "label5";
             // 
@@ -139,10 +144,10 @@
             this.btnGuardar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(137)))), ((int)(((byte)(45)))));
             this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnGuardar.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGuardar.Location = new System.Drawing.Point(575, 497);
-            this.btnGuardar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnGuardar.Location = new System.Drawing.Point(431, 404);
+            this.btnGuardar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnGuardar.Name = "btnGuardar";
-            this.btnGuardar.Size = new System.Drawing.Size(311, 62);
+            this.btnGuardar.Size = new System.Drawing.Size(233, 50);
             this.btnGuardar.TabIndex = 10;
             this.btnGuardar.Text = "GUARDAR";
             this.btnGuardar.UseVisualStyleBackColor = true;
@@ -152,36 +157,39 @@
             // 
             this.labelPlato.AutoSize = true;
             this.labelPlato.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F);
-            this.labelPlato.Location = new System.Drawing.Point(718, 227);
+            this.labelPlato.Location = new System.Drawing.Point(538, 184);
+            this.labelPlato.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelPlato.Name = "labelPlato";
-            this.labelPlato.Size = new System.Drawing.Size(0, 42);
+            this.labelPlato.Size = new System.Drawing.Size(0, 36);
             this.labelPlato.TabIndex = 11;
             // 
             // labelBebida
             // 
             this.labelBebida.AutoSize = true;
             this.labelBebida.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F);
-            this.labelBebida.Location = new System.Drawing.Point(718, 289);
+            this.labelBebida.Location = new System.Drawing.Point(538, 235);
+            this.labelBebida.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelBebida.Name = "labelBebida";
-            this.labelBebida.Size = new System.Drawing.Size(0, 42);
+            this.labelBebida.Size = new System.Drawing.Size(0, 36);
             this.labelBebida.TabIndex = 12;
             // 
             // labelMonto
             // 
             this.labelMonto.AutoSize = true;
             this.labelMonto.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F);
-            this.labelMonto.Location = new System.Drawing.Point(448, 430);
+            this.labelMonto.Location = new System.Drawing.Point(336, 349);
+            this.labelMonto.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelMonto.Name = "labelMonto";
-            this.labelMonto.Size = new System.Drawing.Size(0, 42);
+            this.labelMonto.Size = new System.Drawing.Size(0, 36);
             this.labelMonto.TabIndex = 13;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(405, 27);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(304, 22);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(245, 166);
+            this.pictureBox1.Size = new System.Drawing.Size(184, 135);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -190,9 +198,10 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Mongolian Baiti", 23.25F, System.Drawing.FontStyle.Bold);
-            this.label5.Location = new System.Drawing.Point(378, 432);
+            this.label5.Location = new System.Drawing.Point(284, 351);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(64, 41);
+            this.label5.Size = new System.Drawing.Size(52, 33);
             this.label5.TabIndex = 14;
             this.label5.Text = "S/.";
             this.label5.UseWaitCursor = true;
@@ -201,9 +210,10 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Mongolian Baiti", 23.25F, System.Drawing.FontStyle.Bold);
-            this.label6.Location = new System.Drawing.Point(37, 353);
+            this.label6.Location = new System.Drawing.Point(28, 287);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(321, 41);
+            this.label6.Size = new System.Drawing.Size(263, 33);
             this.label6.TabIndex = 15;
             this.label6.Text = "fecha de Registro:";
             this.label6.UseWaitCursor = true;
@@ -212,18 +222,19 @@
             // 
             this.labelFecha.AutoSize = true;
             this.labelFecha.Font = new System.Drawing.Font("Mongolian Baiti", 23.25F, System.Drawing.FontStyle.Bold);
-            this.labelFecha.Location = new System.Drawing.Point(415, 353);
+            this.labelFecha.Location = new System.Drawing.Point(311, 287);
+            this.labelFecha.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelFecha.Name = "labelFecha";
-            this.labelFecha.Size = new System.Drawing.Size(120, 41);
+            this.labelFecha.Size = new System.Drawing.Size(99, 33);
             this.labelFecha.TabIndex = 16;
             this.labelFecha.Text = "label7";
             // 
             // MantenedorPedidos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(244)))), ((int)(((byte)(222)))));
-            this.ClientSize = new System.Drawing.Size(972, 585);
+            this.ClientSize = new System.Drawing.Size(729, 475);
             this.Controls.Add(this.labelFecha);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -239,7 +250,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MantenedorPedidos";
             this.Text = "MantenedorPedidos";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
