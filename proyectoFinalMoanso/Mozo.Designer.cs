fa827d7ab5f6ef5d76f50966e10befc92783fbe9
Jonatan Laureano <N00176041@upn.pe>
2022-11-30 02:55:28 -0500

    pago

--- proyectoFinalMoanso/Mozo.Designer.cs
+++ proyectoFinalMoanso/Mozo.Designer.cs
@@ -68,9 +68,9 @@
             this.panel1.Controls.Add(this.btnCerrar);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1000, 33);
+            this.panel1.Size = new System.Drawing.Size(750, 27);
             this.panel1.TabIndex = 2;
             this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
             // 
@@ -78,10 +78,10 @@
             // 
             this.btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btn.Image = ((System.Drawing.Image)(resources.GetObject("btn.Image")));
-            this.btn.Location = new System.Drawing.Point(895, 2);
-            this.btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn.Location = new System.Drawing.Point(671, 2);
+            this.btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn.Name = "btn";
-            this.btn.Size = new System.Drawing.Size(48, 25);
+            this.btn.Size = new System.Drawing.Size(36, 20);
             this.btn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.btn.TabIndex = 4;
             this.btn.TabStop = false;
@@ -90,10 +90,10 @@
             // 
             this.btnCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCerrar.Image = global::proyectoFinalMoanso.Properties.Resources.exis;
-            this.btnCerrar.Location = new System.Drawing.Point(949, 2);
-            this.btnCerrar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCerrar.Location = new System.Drawing.Point(712, 2);
+            this.btnCerrar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCerrar.Name = "btnCerrar";
-            this.btnCerrar.Size = new System.Drawing.Size(48, 25);
+            this.btnCerrar.Size = new System.Drawing.Size(36, 20);
             this.btnCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.btnCerrar.TabIndex = 2;
             this.btnCerrar.TabStop = false;
@@ -106,19 +106,20 @@
             this.panelLogo.Controls.Add(this.label1);
             this.panelLogo.Controls.Add(this.pictureBox1);
             this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelLogo.Location = new System.Drawing.Point(0, 33);
-            this.panelLogo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panelLogo.Location = new System.Drawing.Point(0, 27);
+            this.panelLogo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panelLogo.Name = "panelLogo";
-            this.panelLogo.Size = new System.Drawing.Size(1000, 127);
+            this.panelLogo.Size = new System.Drawing.Size(750, 103);
             this.panelLogo.TabIndex = 3;
             // 
             // horaMozo
             // 
             this.horaMozo.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.horaMozo.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.horaMozo.Location = new System.Drawing.Point(661, 26);
+            this.horaMozo.Location = new System.Drawing.Point(496, 21);
+            this.horaMozo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.horaMozo.Name = "horaMozo";
-            this.horaMozo.Size = new System.Drawing.Size(267, 71);
+            this.horaMozo.Size = new System.Drawing.Size(200, 58);
             this.horaMozo.TabIndex = 4;
             this.horaMozo.Text = "00:00:00";
             this.horaMozo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -128,9 +129,10 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(520, 33);
+            this.label1.Location = new System.Drawing.Point(390, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(148, 58);
+            this.label1.Size = new System.Drawing.Size(118, 46);
             this.label1.TabIndex = 5;
             this.label1.Text = "Hora:";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -140,9 +142,9 @@
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
             this.pictureBox1.Image = global::proyectoFinalMoanso.Properties.Resources.logoHorizontal;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(269, 127);
+            this.pictureBox1.Size = new System.Drawing.Size(202, 103);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -158,10 +160,10 @@
             this.panel2.Controls.Add(this.panelContol);
             this.panel2.Controls.Add(this.panel7);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 160);
-            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Location = new System.Drawing.Point(0, 130);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1000, 628);
+            this.panel2.Size = new System.Drawing.Size(750, 510);
             this.panel2.TabIndex = 4;
             // 
             // panelContol
@@ -175,18 +177,17 @@
             this.panelContol.Controls.Add(this.btnPedido);
             this.panelContol.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelContol.Location = new System.Drawing.Point(0, 0);
-            this.panelContol.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panelContol.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panelContol.Name = "panelContol";
-            this.panelContol.Size = new System.Drawing.Size(1000, 628);
+            this.panelContol.Size = new System.Drawing.Size(750, 510);
             this.panelContol.TabIndex = 2;
             // 
             // pictureBox5
             // 
             this.pictureBox5.Image = global::proyectoFinalMoanso.Properties.Resources._2048745;
-            this.pictureBox5.Location = new System.Drawing.Point(392, 266);
-            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox5.Location = new System.Drawing.Point(294, 216);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(199, 100);
+            this.pictureBox5.Size = new System.Drawing.Size(149, 81);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox5.TabIndex = 11;
             this.pictureBox5.TabStop = false;
@@ -194,10 +195,9 @@
             // pictureBox4
             // 
             this.pictureBox4.Image = global::proyectoFinalMoanso.Properties.Resources.descarga__1_;
-            this.pictureBox4.Location = new System.Drawing.Point(655, 50);
-            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox4.Location = new System.Drawing.Point(491, 41);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(148, 87);
+            this.pictureBox4.Size = new System.Drawing.Size(111, 71);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 10;
             this.pictureBox4.TabStop = false;
@@ -205,10 +205,9 @@
             // pictureBox3
             // 
             this.pictureBox3.Image = global::proyectoFinalMoanso.Properties.Resources.images__2_;
-            this.pictureBox3.Location = new System.Drawing.Point(211, 50);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox3.Location = new System.Drawing.Point(158, 41);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(112, 87);
+            this.pictureBox3.Size = new System.Drawing.Size(84, 71);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 9;
             this.pictureBox3.TabStop = false;
@@ -216,10 +215,10 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::proyectoFinalMoanso.Properties.Resources.apagado;
-            this.pictureBox2.Location = new System.Drawing.Point(855, 610);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox2.Location = new System.Drawing.Point(641, 496);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(133, 96);
+            this.pictureBox2.Size = new System.Drawing.Size(100, 78);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 8;
             this.pictureBox2.TabStop = false;
@@ -234,10 +233,10 @@
             this.btnCliente.ForeColor = System.Drawing.Color.Black;
             this.btnCliente.Image = global::proyectoFinalMoanso.Properties.Resources.clientexd;
             this.btnCliente.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnCliente.Location = new System.Drawing.Point(144, 144);
-            this.btnCliente.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCliente.Location = new System.Drawing.Point(108, 117);
+            this.btnCliente.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCliente.Name = "btnCliente";
-            this.btnCliente.Size = new System.Drawing.Size(260, 76);
+            this.btnCliente.Size = new System.Drawing.Size(195, 62);
             this.btnCliente.TabIndex = 3;
             this.btnCliente.Text = "Cliente";
             this.btnCliente.UseVisualStyleBackColor = true;
@@ -252,10 +251,10 @@
             this.btnPedidos.ForeColor = System.Drawing.Color.Black;
             this.btnPedidos.Image = global::proyectoFinalMoanso.Properties.Resources.billetera;
             this.btnPedidos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnPedidos.Location = new System.Drawing.Point(291, 372);
-            this.btnPedidos.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnPedidos.Location = new System.Drawing.Point(218, 302);
+            this.btnPedidos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnPedidos.Name = "btnPedidos";
-            this.btnPedidos.Size = new System.Drawing.Size(375, 82);
+            this.btnPedidos.Size = new System.Drawing.Size(281, 67);
             this.btnPedidos.TabIndex = 1;
             this.btnPedidos.Text = "Formas de pago";
             this.btnPedidos.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -271,10 +270,10 @@
             this.btnPedido.ForeColor = System.Drawing.Color.Black;
             this.btnPedido.Image = global::proyectoFinalMoanso.Properties.Resources._32;
             this.btnPedido.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnPedido.Location = new System.Drawing.Point(616, 138);
-            this.btnPedido.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnPedido.Location = new System.Drawing.Point(462, 112);
+            this.btnPedido.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnPedido.Name = "btnPedido";
-            this.btnPedido.Size = new System.Drawing.Size(260, 82);
+            this.btnPedido.Size = new System.Drawing.Size(195, 67);
             this.btnPedido.TabIndex = 0;
             this.btnPedido.Text = "Pedido";
             this.btnPedido.UseVisualStyleBackColor = true;
@@ -283,21 +282,21 @@
             // panel7
             // 
             this.panel7.Location = new System.Drawing.Point(0, 0);
-            this.panel7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(16, 839);
+            this.panel7.Size = new System.Drawing.Size(12, 682);
             this.panel7.TabIndex = 1;
             // 
             // Mozo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1000, 788);
+            this.ClientSize = new System.Drawing.Size(750, 640);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panelLogo);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Mozo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Mozo";
